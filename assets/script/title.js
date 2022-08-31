70c9786d8d1372f3b7a6ab6157323f97b3a84d12
dxtstd <denta123.ft@gmail.com>
2022-08-31 09:35:57 +0800

    first commit

---
+++ assets/script/title.js
@@ -0,0 +1,27 @@
+async function sleep(s) {
+    return new Promise((res, rej) => {
+        setTimeout(res, s*1000)
+    })
+}
+
+const title = [
+    "DentaCH",
+    "under development"
+];
+let indexTitle = 0
+
+async function changeTitle () {
+    document.title = title[indexTitle];
+    indexTitle ++
+    if (indexTitle > (title.length - 1)) {
+        indexTitle = 0
+    }
+}
+
+async function loopChangeTitle () {
+    changeTitle()
+    await sleep(5)
+    loopChangeTitle()
+}
+
+loopChangeTitle()
