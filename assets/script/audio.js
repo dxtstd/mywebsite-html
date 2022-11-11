7667f0606db2bf07bb4dd27f417b3ba68536b795
dxtstd <denta123.ft@gmail.com>
2022-11-12 07:58:30 +0800

    fix audio

--- assets/script/audio.js
+++ assets/script/audio.js
@@ -1,6 +1,16 @@
 (function() {
-    window.addEventListener('load', function () {
+    let isPlayed = false
+    const Timeout = 2*1000
+    const play = function () {
         const audio_1 = document.getElementById('audio_1')
         audio_1.play()
+    }
+    window.addEventListener('load', function () {
+        window.addEventListener('click', function () {
+            if (!isPlayed) {
+                setTimeout(play, Timeout)
+                isPlayed = true
+            }
+        })
     })
 })();
