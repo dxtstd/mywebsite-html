a687841710d40cbd1664ff1237a467a92b946b0f
dxtstd <denta123.ft@gmail.com>
2022-11-14 01:16:15 +0800

    fix

--- assets/script/audio.js
+++ assets/script/audio.js
@@ -3,19 +3,21 @@
     const Timeout = 1*1000
     const play = function () {
         const audio_1 = document.getElementById('audio_1')
-        audio_1.play()
+        audio_1.play().then(() => {
+            isPlayed = true
+        }).catch(err => {
+            console.error(err)
+        })
     }
     window.addEventListener('load', function () {
         window.addEventListener('click', function () {
             if (!isPlayed) {
                 setTimeout(play, Timeout)
-                isPlayed = true
             }
         })
         window.addEventListener('scroll', function () {
             if (!isPlayed) {
                 setTimeout(play, Timeout)
-                isPlayed = true
             }
         })
     })
